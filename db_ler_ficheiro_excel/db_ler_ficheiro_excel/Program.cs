d40706371ccdf4ea7f37c595123a31a7a47e088d
Andre Pinheiro <sc0rched@hotmail.com>
2011-05-21 22:06:12 +0100

    Le o ficheiro excel

---
+++ db_ler_ficheiro_excel/db_ler_ficheiro_excel/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using OfficeOpenXml;
+
+namespace db_ler_ficheiro_excel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //isto tem que ser melhorado aqui. mas deixamos essa parte para quando incluirmos a parte gráfica. 
+                // o user deve escolher o ficheiro .xls/xlsx que quiser
+                DirectoryInfo directorio = new DirectoryInfo(@"C:\Users\Alfredo Pinheiro\Desktop\db_linha_de_comandos\db_linha_de_comandos\bin\Debug\blah.xlsx");
+                string output = @"C:\Users\Alfredo Pinheiro\Desktop\db_linha_de_comandos\db_linha_de_comandos\bin\Debug\blah.xlsx";
+
+                Console.WriteLine("A extrair dados...");
+                ler_ficheiro_excel.ler_ficheiro(output);
+            }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine("Erro {0}", ex.Message);
+            }
+
+            Console.WriteLine();
+            Console.Read();
+        }
+    }
+}
---
+++ db_ler_ficheiro_excel/db_ler_ficheiro_excel/ler_ficheiro_excel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OfficeOpenXml;
+using System.IO;
+
+namespace db_ler_ficheiro_excel
+{
+    class ler_ficheiro_excel
+    {
+        public static void ler_ficheiro(string pasta_ficheiro)
+        {
+            FileInfo _ficheiro = new FileInfo(pasta_ficheiro);
+
+            using (ExcelPackage _package = new ExcelPackage(_ficheiro))  //acho que já entendeste o que isto faz
+            {
+                //dentro do workbook vamos apenas utilizar a primeira(e unica) folha que fizemos anteriormente
+                //a folha com o nome "Alunos"
+                ExcelWorksheet ws = _package.Workbook.Worksheets[1];
+
+                int coluna = 2;     //a coluna do ano de escolaridade
+                for (int linha = 2; linha < 4; linha++)
+                    Console.WriteLine("Célula({0}, {1}). Valor = {2}", linha, coluna, ws.Cells[linha, coluna].Value);
+            }
+
+            Console.WriteLine("Feito...Dados extraidos");
+        }
+    }
+}
